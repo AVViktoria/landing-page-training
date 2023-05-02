91a833215525cfc6188ad74e584fda4b6d15d1cd
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-05-02 16:47:11 -0700

    feat: add button to top

--- js/main.js
+++ js/main.js
@@ -25,3 +25,24 @@ const lightbox = new SimpleLightbox('.gallery-link a', {});
 //*             titles              *//
 
 AOS.init();
+
+
+// / Get the button:
+let myButtonScroll = document.getElementById("myButtonScroll");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myButtonScroll.style.display = "block";
+  } else {
+    myButtonScroll.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
